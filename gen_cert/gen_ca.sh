b82f4c5ecc74fdee248a4c7031000b47a753ea33
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2021-10-10 14:23:03 +0300

    failed

--- gen_cert/gen_ca.sh
+++ gen_cert/gen_ca.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=yngwie proxy CA"
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=yngwie proxy CA" -config san.cnf -config san.cnf -extensions v3_req
 openssl genrsa -out cert.key 2048
 mkdir certs/
