186e6eff0e3cc346b26a82784f7377e5d1e10a61
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2021-10-10 13:18:43 +0300

    console work

--- gen_cert/gen_cert.sh
+++ gen_cert/gen_cert.sh
@@ -1,1 +1,2 @@
-openssl req -new -key cert.key -subj "/CN=$1" -sha256 | openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -set_serial "$2"
+#!/bin/sh
+openssl req -new -key ./gen_cert/cert.key -subj "/CN=$1" -sha256 | openssl x509 -req -days 3650 -CA ./gen_cert/ca.crt -CAkey ./gen_cert/ca.key -set_serial "$2" > ./gen_cert/certs/"$1".crt
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
 	"io"
 	"log"
-	"net"
+	"math/rand"
 	"net/http"
+	"net/http/httputil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 const (
 	port      = ":8080"
 	pemFile = "gen_cert/ca.crt"
-	keyFile = "gen_cert/ca.key"
+	keyCAFile = "gen_cert/ca.key"
+	keyCertFile = "gen_cert/cert.key"
+	certDir = "gen_cert/certs/"
 )
 
 
@@ -26,7 +34,6 @@ func newServer(port string) *Server {
 			Addr: port,
 			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 				if r.Method == http.MethodConnect {
-					log.Println(1)
 					handleHTTPS(w, r)
 				} else {
 					handleHTTP(w, r)
@@ -36,40 +43,131 @@ func newServer(port string) *Server {
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
 	}
 }
+func getCert(host string) (tls.Certificate, error){
+	_, err := os.Stat(certDir + host + ".crt")
+	if os.IsNotExist(err) {
+		log.Println("start generating cert")
+		genCommand := exec.Command("gen_cert/gen_cert.sh", host, strconv.Itoa(rand.Intn(1000000)))
+
+		_, err = genCommand.CombinedOutput()
+		if err != nil {
+			log.Println("error executing command getting cert")
+			log.Println(err)
+			return tls.Certificate{}, err
+		}
+	}
+
+	file, err := tls.LoadX509KeyPair(certDir + host + ".crt", keyCertFile)
+	if err != nil {
+		log.Println("error loading pair", err)
+		return tls.Certificate{}, err
+	}
+
+	return file, nil
+}
 
 func handleHTTPS(w http.ResponseWriter, r *http.Request) {
-	log.Println("connect")
-	dest_conn, err := net.Dial("tcp", r.Host)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-
+	// initialize tcp client
+	// take over connection
 	hijacker, ok := w.(http.Hijacker)
 	if !ok {
+		log.Println("Hijacking not supported")
 		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
 		return
 	}
-	client_conn, _, err := hijacker.Hijack()
+
+	clientConn, _, err := hijacker.Hijack()
 	if err != nil {
+		log.Println("hijacking error", err)
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 	}
-	go transfer(dest_conn, client_conn)
-	go transfer(client_conn, dest_conn)
 
-}
-func transfer(destination io.WriteCloser, source io.ReadCloser) {
-	defer destination.Close()
-	defer source.Close()
-	io.Copy(destination, source)
+	_, err = clientConn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+	if err != nil {
+		log.Println("handshaking failed", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		clientConn.Close()
+		return
+	}
+	defer clientConn.Close()
+
+	// get host
+	host := strings.Split(r.Host, ":")[0]
+
+	// get certs
+	caCert, err := getCert(host)
+	if err != nil {
+		log.Println("error getting cert")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// create config
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{caCert},
+		ServerName: r.URL.Scheme,
+	}
+
+	// create conn with config
+	tcpClient := tls.Server(clientConn, tlsConfig)
+	err = tcpClient.Handshake()
+	if err != nil {
+		tcpClient.Close()
+		log.Println("handshaking failed", err)
+		return
+	}
+	defer tcpClient.Close()
+
+	// create tcp server
+	tcpServer, err := tls.Dial("tcp", r.URL.Host, tlsConfig)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer tcpServer.Close()
+
+	// get request
+	clientReader := bufio.NewReader(tcpClient)
+	request, err := http.ReadRequest(clientReader)
+	if err != nil {
+		log.Println("error getting request", err)
+		return
+	}
+
+	//serve request
+	dumpRequest, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		log.Println("fail to dump request", err)
+		return
+	}
+	_, err = tcpServer.Write(dumpRequest)
+	if err != nil {
+		log.Println("fail to write request: ", err)
+		return
+	}
+
+	serverReader := bufio.NewReader(tcpServer)
+	response, err := http.ReadResponse(serverReader, request)
+	if err != nil {
+		log.Println("fail to read response: ", err)
+		return
+	}
+
+	rawResponse, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		log.Println("fail to dump response: ", err)
+		return
+	}
+
+	_, err = tcpClient.Write(rawResponse)
+	if err != nil {
+		log.Println("fail to write response: ", err)
+		return
+	}
 }
 
 func handleHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Proto != "HTTP/1.1" {
-		http.Error(w, "Http support only", http.StatusForbidden)
-	}
-
 	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
